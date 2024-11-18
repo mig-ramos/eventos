168288a076665bfde6c654c8b23647438bbe68bb
mig-ramos <mig.ramos10@gmail.com>
2024-11-18 16:30:10 -0300

    cria componente Pagina - parte 1

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import Pagina from "@/components/template/Pagina";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Início</div>
+    <Pagina>
+      <div>Início</div>
+    </Pagina>
   );
 }
---
+++ apps/frontend/src/components/template/Pagina.tsx
@@ -0,0 +1,21 @@
+export interface PaginaProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Pagina(props: PaginaProps) {
+  return (
+    <div
+      className="
+        flex flex-col items-center py-10 min-h-screen
+        bg-[url('/background.png')] bg-cover
+      "
+    >
+      <main
+        className={props.className}
+      >
+        {props.children}
+      </main>
+    </div>
+  );
+}
