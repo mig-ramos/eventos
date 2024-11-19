57aaa222d6045bf59ba4390ecde5dd4924d9c27f
mig-ramos <mig.ramos10@gmail.com>
2024-11-19 17:28:35 -0300

    cria page inicial e rotas

---
+++ apps/frontend/src/components/template/LogoGrande.tsx
@@ -0,0 +1,20 @@
+import { Righteous } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import "../../app/globals.css"
+
+const fonte = Righteous({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export default function LogoGrande() {
+  return (
+    <Link href="/" className={`flex flex-col items-center gap-2 ${fonte.className}`}>
+      <Image src="/logo.svg" width={100} height={100} alt="Logo" />
+      <h1 className="text-5xl text-white">
+          CONVIT<span className="text-blue-500">3</span> DIGITAL
+      </h1>
+    </Link>
+  );
+}
