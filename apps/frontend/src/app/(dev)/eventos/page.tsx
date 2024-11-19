b0b6fc5bd43cee089f9ba891ee9746b5190cd03e
mig-ramos <mig.ramos10@gmail.com>
2024-11-19 20:12:29 -0300

    instla qr-code

--- apps/frontend/src/app/(dev)/eventos/page.tsx
+++ apps/frontend/src/app/(dev)/eventos/page.tsx
@@ -1,7 +1,7 @@
 import { eventos } from "@/core";
 import Image from "next/image";
 import Link from "next/link";
-// import QRCode from "react-qr-code";
+import QRCode from "react-qr-code";
 
 export default function PaginaEventos() {
   return (
@@ -25,10 +25,10 @@ export default function PaginaEventos() {
           <div className="flex-1 flex flex-col items-center p-7 gap-5 text-center">
             <span className="text-lg font-black text-white">{evento.nome}</span>
             <p className="flex-1 text-sm text-zinc-400">{evento.descricao}</p>
-            {/* <QRCode
+            <QRCode
               value={JSON.stringify({ id: evento.id, senha: evento.senha })}
               className="w-44 h-44"
-            /> */}
+            />
             <div className="flex gap-5">
               <Link
                 href={`/evento/admin/${evento.id}/${evento.senha}`}
